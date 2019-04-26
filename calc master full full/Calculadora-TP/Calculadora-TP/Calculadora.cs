b3bb971f30013016807d068c97e7fada11da5779
nicoleduarte <48369150+nicoleduarte@users.noreply.github.com>
2019-04-26 19:37:49 -0300

    Add files via upload

---
+++ calc master full full/Calculadora-TP/Calculadora-TP/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_TP
+{
+    class Calculadora
+    {
+        //Esta classe define o que a calculadora deve fazer
+
+        //Estes são os atributos que a nossa classe Calculadora têm para fazer qualquer conta.
+        //n1 equivale ao primeiro número da conta e n2 ao segundo.
+        private double n1;
+        private double n2;
+
+        //Estes são os valores encapsulados.
+        public double N1 { get => n1; set => n1 = value; }
+        public double N2 { get => n2; set => n2 = value; }
+
+
+        //A classe calculadora efetua as contas recebendo os valores de n1 e n2 do formulário
+        //depois de receber os valores o método Soma() é capaz de somar os valores armazenados no objeto soma e retornar o resultado da soma.
+        public double Soma()
+        {
+            double soma = this.N1 + this.N2;
+            return (soma);
+        }
+        public double Subtrai()
+        {
+            double resultado = this.N1 - this.N2;
+            return (resultado);
+        }
+
+        public double Dividir()
+        {
+            double resultado = this.N1 / this.N2;
+            return (resultado);
+        }
+
+        public double Multiplicar()
+        {
+            double resultado = this.N1 * this.N2;
+            return (resultado);
+        }
+
+        public double Elevar()
+        {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            return (Math.Pow(this.N1, this.N2));
+        }
+    }
+}
---
+++ calc master full full/Calculadora-TP/Calculadora-TP/frmCalc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_TP
+{
+    public partial class frmCalc : Form
+    {
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalc_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.N1 = Double.Parse(txtN1.Text);
+            calc.N2 = Double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calc.Multiplicar());
+        }
+
+        private void btnExpo_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.N1 = Double.Parse(txtN1.Text);
+            calc.N2 = Double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calc.Elevar());                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.N1 = Double.Parse(txtN1.Text);
+            calc.N2 = Double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calc.Soma());
+
+        }
+
+        private void btnSubtrai_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.N1 = double.Parse(txtN1.Text);
+            calc.N2 = double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calc.Subtrai());
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            Calculadora calc = new Calculadora();
+            calc.N1 = double.Parse(txtN1.Text);
+            calc.N2 = double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calc.Dividir());
+        }
+    }
+}
